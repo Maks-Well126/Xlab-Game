03dfc1623ebd71c9f5a30ecdf486ac651ffa3550
Maksim Korotenko <mskorotenko@mail.ru>
2026-02-27 13:36:23 +0300

    ServiceLocator

---
+++ Assets/Scripts/Infrastructure/ServiceLocatot.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class ServiceLocator
+{
+    private static ServiceLocator m_serviceLocator;
+
+    private Dictionary<Type, object> m_services = new();
+
+    public static void Register<T>(T instance )
+        where T : class
+    {
+        m_serviceLocator ??= new ServiceLocator();
+        m_serviceLocator.m_services.Add(typeof(T), instance);   
+        
+    }
+
+    public static T Resolve<T>()
+        where T : class
+    {
+        if(m_serviceLocator is null)
+            throw new NullReferenceException("Service Locator is null");
+
+            return m_serviceLocator.m_services[typeof(T)] as T;
+    }
+
+}
